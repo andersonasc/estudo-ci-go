307756f46427ca6675d887a04ef356f322090e01
anderson.clemente <anderson.clementesi@gmail.com>
2024-11-12 22:04:06 -0300

    Test pull request

--- math_test.go
+++ math_test.go
@@ -6,6 +6,6 @@ func TestSoma(t *testing.T) {
 	total := Soma(20, 20)
 
 	if total != 40 {
-		t.Errorf("Resultado da soma é inválido : Resultado %d. Esperado: %d", total, 40)
+		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 40)
 	}
 }
